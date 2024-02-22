3256cfe8094aafe9c02e697f45e0931aeb48e6f5
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-22 16:08:42 +0530

    Remove link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -5,7 +5,6 @@ function Link(props: HTMLProps<HTMLAnchorElement>) {
 	return (
 		<a
 			target="_blank"
-			href="https://github.com/leoMirandaa"
 			{...props}
 			className={cn("text-primary transition-all border-primary hover:border-b-2", props.className)}
 		></a>
