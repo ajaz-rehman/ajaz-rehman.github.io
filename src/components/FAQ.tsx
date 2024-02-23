0f8ba87bbff4bcab19dac54a8bae72916c0290fb
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-23 13:53:20 +0530

    Refactor faq data

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,40 +1,7 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import Link from "./Link";
+import { FAQ_LIST } from "@/data/faq";
 import { SOCIAL } from "@/data/links";
-
-interface FAQProps {
-	question: string;
-	answer: string;
-	value: string;
-}
-
-const FAQList: FAQProps[] = [
-	{
-		question: "Is this template free?",
-		answer: "Yes. It is a free ChadcnUI template.",
-		value: "item-1",
-	},
-	{
-		question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
-		answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint labore quidem quam? Consectetur sapiente iste rerum reiciendis animi nihil nostrum sit quo, modi quod.",
-		value: "item-2",
-	},
-	{
-		question: "Lorem ipsum dolor sit amet  Consectetur natus dolores minus quibusdam?",
-		answer: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore qui nostrum reiciendis veritatis necessitatibus maxime quis ipsa vitae cumque quo?",
-		value: "item-3",
-	},
-	{
-		question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit?",
-		answer: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-		value: "item-4",
-	},
-	{
-		question: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur natus?",
-		answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint labore quidem quam? Consectetur sapiente iste rerum reiciendis animi nihil nostrum sit quo, modi quod.",
-		value: "item-5",
-	},
-];
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 export const FAQ = () => {
 	return (
@@ -47,7 +14,7 @@ export const FAQ = () => {
 			</h2>
 
 			<Accordion type="single" collapsible className="w-full AccordionRoot">
-				{FAQList.map(({ question, answer, value }: FAQProps) => (
+				{FAQ_LIST.map(({ question, answer, value }) => (
 					<AccordionItem key={value} value={value}>
 						<AccordionTrigger className="text-left">{question}</AccordionTrigger>
 
