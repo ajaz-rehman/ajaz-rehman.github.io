1547c9a17e5fa5f570ab19b1c667b46758214d94
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-26 15:09:53 +0530

    Change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,16 @@
-import { SOURCE_CODE, WEBSITE } from "@/data/links";
+import { SOURCE_CODE } from "@/data/links";
 import Link from "./Link";
+import SocialLinks from "./SocialLinks";
 
 export const Footer = () => {
 	return (
 		<footer id="footer">
 			<hr className="w-11/12 mx-auto" />
 
-			<section className="container py-10 text-center">
+			<section className="container py-10 flex flex-col lg:flex-row gap-8 text-center items-center justify-between">
+				<SocialLinks />
 				<h3>
-					Built by{" "}
-					<Link target="_self" href={WEBSITE}>
-						Ajaz Ur Rehman
-					</Link>
-					. The source code is available on <Link href={SOURCE_CODE}>GitHub</Link>.
+					Source code is available on <Link href={SOURCE_CODE}>GitHub</Link>.
 				</h3>
 			</section>
 		</footer>
