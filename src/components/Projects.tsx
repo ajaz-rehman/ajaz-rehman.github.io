2942d1fd1c1dc4331f1a413969624371cb225aba
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-26 13:06:25 +0530

    Show images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,11 +11,15 @@ export const Projects = () => {
 	return (
 		<section id="projects" className="container py-24 sm:py-32 space-y-8">
 			<h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-				Client{" "}
+				My Recent{" "}
 				<span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
 					Projects
 				</span>
 			</h2>
+
+			<p className="text-muted-foreground text-xl !mt-4 !mb-0 text-center">
+				Here are a few past projects I've worked on.
+			</p>
 
 			<div className="flex flex-wrap md:justify-center gap-4">
 				{PROJECTS.map(project => (
@@ -40,10 +44,12 @@ export const Projects = () => {
 					<CardDescription className="text-md">{activeProject.companyDescription}</CardDescription>
 				</CardHeader>
 
-				<CardContent></CardContent>
+				<CardContent>
+					<img src={activeProject.imageSrc} className="h-auto max-w-full rounded-lg border" />
+				</CardContent>
 
 				<CardFooter className="justify-center">
-					<Link href={activeProject.websiteLink}>Open website</Link>
+					<Link href={activeProject.websiteLink}>Visit website</Link>
 				</CardFooter>
 			</Card>
 		</section>
