645bf57d008485ca20b36e6ead884a070995617d
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-28 11:04:33 +0530

    Fix footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
-import { SOURCE_CODE } from "@/data/links";
 import Link from "./Link";
 import SocialLinks from "./SocialLinks";
+import { SOURCE_CODE } from "@/data/links";
 
 export const Footer = () => {
 	return (
 		<footer id="footer">
 			<hr className="w-11/12 mx-auto" />
 
-			<section className="container py-10 flex flex-col lg:flex-row gap-8 text-center items-center justify-between">
+			<section className="container py-8 flex flex-col lg:flex-row gap-8 text-center items-center justify-between">
 				<SocialLinks />
 				<h3>
 					Source code is available on <Link href={SOURCE_CODE}>GitHub</Link>.
