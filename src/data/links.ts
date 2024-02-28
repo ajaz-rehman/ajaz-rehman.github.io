a5955229712afdbcb3c50393d16d526f5fbbfd5a
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-28 11:09:29 +0530

    Update link

--- src/data/links.ts
+++ src/data/links.ts
@@ -16,6 +16,6 @@ export const SERVICES = {
 	AWS_CloudFront: "https://aws.amazon.com/cloudfront",
 };
 
-export const SOURCE_CODE = "https://github.com/ajaz-ur-rehman";
+export const SOURCE_CODE = "https://github.com/ajaz-ur-rehman/portfolio";
 
 export const WEBSITE = "/";
