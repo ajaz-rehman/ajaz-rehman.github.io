4e0755c1ec1da71ec8368014c759b58a1ed46ba9
Ajaz Ur Rehman <fateslayer47@gmail.com>
2024-02-28 11:40:19 +0530

    Update hero classes

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -2,8 +2,8 @@ import heroImage from "@/assets/images/hero.png";
 
 export function HeroCards() {
 	return (
-		<div className="hidden lg:flex flex-row flex-wrap gap-8 relative w-[700px] h-[500px]">
-			<img src={heroImage} alt="Hero" className="absolute top-0 right-0 h-full" />
+		<div className="hidden lg:block">
+			<img src={heroImage} alt="Hero" className="w-full" />
 		</div>
 	);
 }
