d2b92cc79d6132d570f75685a7b9a3082179114d
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-06-30 07:10:06 +0000

    Update resume

--- src/data/links.ts
+++ src/data/links.ts
@@ -5,7 +5,7 @@ export const SOCIAL = {
 	email: "mailto:ajazurrehman4@gmail.com",
 };
 
-export const RESUME = "/resume.pdf";
+export const RESUME = "/Ajaz Ur Rehman - Software Engineer - Resume.pdf";
 
 export const SERVICES = {
 	Vite: "https://vitejs.dev",
