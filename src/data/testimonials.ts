e54496ba0c74c8569ead2d9f96c1d3dd5a770418
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-08-13 09:56:41 +0000

    Reorder testimonials

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -5,32 +5,6 @@ import diparatiImage from "@/assets/images/testimonials/diparati.jpeg";
 
 export const TESTIMONIALS = [
 	{
-		image: diparatiImage,
-		name: "Diparati Sen",
-		position: "Product Designer",
-		socialNetworks: [
-			{ name: "Linkedin", url: "https://linkedin.com/in/diparatisen" },
-			{
-				name: "Instagram",
-				url: "https://instagram.com/__a.girl.can.dream__",
-			},
-		],
-		text: "Ajaz makes sure he understands the work before he starts working. He has an amazing eye for detail and asks the right questions. He is one of the most patient and dedicated people I have come across and I can blindly recommend him because he will surely be an asset.",
-	},
-	{
-		image: lucasImage,
-		name: "Lucas Infante",
-		position: "Software Developer",
-		socialNetworks: [
-			{ name: "Linkedin", url: "https://linkedin.com/in/lucasinfante" },
-			{
-				name: "Instagram",
-				url: "https://instagram.com/lucasinfante",
-			},
-		],
-		text: "Ajaz is a great guy and an awesome developer. Besides having good work ethics, programming knowledge, and problem solving skills, he also learns and adapts quickly and has a keen attention to detail. Having Ajaz in the team is an advantage, totally recommend him!",
-	},
-	{
 		image: lisaImage,
 		name: "Lisa Schumann",
 		position: "Front-End Lead",
@@ -55,5 +29,31 @@ export const TESTIMONIALS = [
 			},
 		],
 		text: "Ajaz is one of those rare developers you come across in life when it comes to superb skills & problem solving. He is incredibly quick to learn and adapt anything you throw at his way. Having worked with him for last 6 months, I can vouch for his dedication and passion.",
+	},
+	{
+		image: lucasImage,
+		name: "Lucas Infante",
+		position: "Software Developer",
+		socialNetworks: [
+			{ name: "Linkedin", url: "https://linkedin.com/in/lucasinfante" },
+			{
+				name: "Instagram",
+				url: "https://instagram.com/lucasinfante",
+			},
+		],
+		text: "Ajaz is a great guy and an awesome developer. Besides having good work ethics, programming knowledge, and problem solving skills, he also learns and adapts quickly and has a keen attention to detail. Having Ajaz in the team is an advantage, totally recommend him!",
+	},
+	{
+		image: diparatiImage,
+		name: "Diparati Sen",
+		position: "Product Designer",
+		socialNetworks: [
+			{ name: "Linkedin", url: "https://linkedin.com/in/diparatisen" },
+			{
+				name: "Instagram",
+				url: "https://instagram.com/__a.girl.can.dream__",
+			},
+		],
+		text: "Ajaz makes sure he understands the work before he starts working. He has an amazing eye for detail and asks the right questions. He is one of the most patient and dedicated people I have come across and I can blindly recommend him because he will surely be an asset.",
 	},
 ];
